aa4f0c558971574c417908942e383f1e7d8bac28
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2013-09-11 19:55:46 +0530

    First Commit of Common Utilities

---
+++ Common Utility/src/org/iiitb/model/bean/Resource.java
@@ -0,0 +1,11 @@
+package org.iiitb.model.bean;
+
+import org.iiitb.model.consts.ResourceType;
+
+public class Resource {
+	int rid;
+	String resourceName;
+	boolean availability;
+	int ownerPid;
+	ResourceType rType;
+}
